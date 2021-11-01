bc48ab66bb238213d9fd90ec3e1c91441b4dc9f3
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 11:17:24 -0300

    MyTreelhas

--- src/pages/MyTreelhas/index.tsx
+++ src/pages/MyTreelhas/index.tsx
@@ -1,9 +1,46 @@
+//import { useState } from "react";
 import { Header } from "../../components/Header";
+import { PageHeader } from "../../components/PageHeader";
+//import {motion, AnimateSharedLayout } from 'framer-motion';
+
+import emptyImg from '../../assets/images/empty.svg';
+
+import { useHistory } from "react-router-dom";
+//import { Modal } from "../../components/Modal";
+
+import './styles.scss';
+//import { Treelha } from "../../components/Treelha";
 
 export function MyTreelhas(){
+
+    const empty =  true;
+    const history = useHistory();
+
+    function handleNewTreelha(){
+        history.push('/treelhas/newtreelha')
+    }
+
     return(
         <>
             <Header/>
+            
+            <main>
+                <PageHeader navigateFunction={handleNewTreelha} title="Minhas trilhas"/>
+                {/* <Treelha name="Pedra do elefante" date="26-10-2021" type="hiking"/> */}
+                { empty ? (
+                    <div className="empty">
+                        <img className="empty-image" src={emptyImg} alt="Nenhuma trilha por enquanto" />
+                        <div className="text">
+                            <h3>Nenhuma trilha por enquanto...</h3>
+                            <p>Crie sua primeira aventura!</p>
+                        </div>
+                    </div>
+                ) : (
+                    <div></div>
+                ) }
+
+            </main>
+            
         </>           
     )
 }
