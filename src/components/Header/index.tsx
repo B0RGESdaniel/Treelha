d10222d2a6f7d7fa58abb4ba0a872c717a882666
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 14:54:26 -0300

    Migrating Header component to styled-components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,13 @@
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-import { useAuth } from '../../hooks/useAuth';
-import { Modal } from '../Modal';
-import './styles.scss';
-
-
-
+import { Container, Content } from './styles';
 
 export function Header(){
-
-    const { user } = useAuth();
-
-    const [showModal, setShowModal] = useState(false);
-
     return(
         <>
-        <div className="header">
-            <div className="content">
-                <motion.h1
-                whileHover={{scale: 1.1, color: '#29B672'}}
-                whileTap={{scale: 0.9, color: '#96C67F'}}
-                >Treelha</motion.h1>
-                <div className="user-info" onClick={() => setShowModal(!showModal)}>
-                    <div className="user-info-content">
-                        <p className="user-name">{user?.name}</p>
-                        <img className="avatar" src={user?.avatar} alt={user?.name} /> 
-                    </div>         
-                    <Modal showModal={showModal} setShowModal={setShowModal}/>
-                </div>
-            </div>  
-        </div>
+        <Container>
+            <Content>
+                <h1>Treelha</h1>
+            </Content>  
+        </Container>
         </>
     )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: transparent;
+    padding: 2px;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1120px;
+    margin: 15px auto;
+    color: var(--green);
+    font-weight: 700;
+`;
