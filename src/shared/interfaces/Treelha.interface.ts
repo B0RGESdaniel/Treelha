b1c4059e7ae714e96201814ebb94c04f45347bb8
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 19:47:24 -0300

    Create Treelha.interface.ts

---
+++ src/shared/interfaces/Treelha.interface.ts
@@ -0,0 +1,10 @@
+export interface Treelha {
+    name: string;
+    type: string;
+    style: string;
+    numberOfParticipants: number;
+    address: string;
+    date: string;
+    time: string;
+    whatsappGroupLink: string;
+}
