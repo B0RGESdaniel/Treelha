22c0c09da3f97df62d75e565f03957bcc9e4a691
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 11:17:06 -0300

    NewTreelha

---
+++ src/pages/NewTreelha/index.tsx
@@ -0,0 +1,84 @@
+import { Header } from "../../components/Header";
+import { PageHeader } from "../../components/PageHeader";
+//import { useForm } from "react-hook-form";
+
+import './styles.scss';
+
+export function NewTreelha(){
+    return(
+        <>
+            <Header/>
+            <PageHeader title="Crie sua trilha"/>
+            <main>
+            <form>
+                <input type="text" className="input name" placeholder="Nome da trilha" />
+                <div className="radio-fieldset">
+                    <label className="radio-title" htmlFor="type">Tipo</label>
+                    <div className="content">
+                        
+                        <label className="radio-container">
+                            <input type="radio" name="type" id="bike" value="bike" />
+                            Bicicleta
+                            <span className="checkmark"></span>
+                        </label>
+
+                        <label className="radio-container">
+                            <input type="radio" name="type" id="walk" value="walk" />
+                            Caminhada
+                            <span className="checkmark"></span>
+                        </label>
+
+                        <label className="radio-container">
+                            <input type="radio" name="type" id="hiking" value="hiking" />
+                            Hiking
+                            <span className="checkmark"></span>
+                        </label>
+                        </div>
+                    </div>
+
+                    <div className="radio-fieldset">
+                        <label className="radio-title" htmlFor="style">Estilo</label>
+                        <div className="content">
+
+                            <label className="radio-container">
+                                <input type="radio" name="style" id="road" value="road" />
+                                Estrada
+                                <span className="checkmark"></span>
+                            </label>
+
+                            <label className="radio-container">
+                                <input type="radio" name="style" id="off-road" value="off-road" />
+                                Off-road
+                                <span className="checkmark"></span>
+                            </label>
+
+                            <label className="radio-container">
+                                <input type="radio" name="style" id="woods" value="woods" />
+                                Mata
+                                <span className="checkmark"></span>
+                            </label>
+                        </div>
+                    </div>
+
+
+                <label htmlFor="people">Nº de pessoas</label>
+                <input type="number" className="input number" placeholder="0" id="people" />
+
+                <label htmlFor="place">Local</label>
+                <input type="text" className="input text" placeholder="Rua dos pombos, 0" id="place"/>
+
+                <label htmlFor="day">Dia</label>
+                <input type="date" className="input date" placeholder="08/02/20002" id="day" />
+
+                <label htmlFor="hour">Hora</label>
+                <input type="time" className="input time" id="hour" />
+
+                <label htmlFor="whatsapp-link">Link do grupo Whatsapp<span>(opcional)</span></label>
+                <input type="text" className="input text" placeholder="www.whatsapp.com.br" id="whatsapp-link" />
+
+                <button type="submit">Salvar</button>
+                </form>
+            </main>
+        </>
+    )
+}
