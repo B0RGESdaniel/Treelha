adb5fb20d4fecb57dcb52e396cc065edc3677cd3
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 11:35:25 -0300

    Migrating global styles to styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import { MyTreelhas } from './pages/MyTreelhas';
 import { NewTreelha } from './pages/NewTreelha';
 import { AuthContextProvider } from './contexts/AuthContext';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/treelhas" exact component={MyTreelhas}/>
-          <Route path="/treelhas/newtreelha" component={NewTreelha}/>
-        </Switch>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/treelhas" exact component={MyTreelhas}/>
+            <Route path="/treelhas/newtreelha" component={NewTreelha}/>
+          </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>
+      <GlobalStyle/>
+    </>
   );
 }
 
