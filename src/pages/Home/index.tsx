85a43879a40dcbd75444f7442da9bf46389cac94
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 12:16:54 -0300

    Migrating Home to styled-components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import googleImg from '../../assets/images/google.svg';
 import logoHomeImg from '../../assets/images/logo-home.svg';
 import { useAuth } from '../../hooks/useAuth';
 
-import './styles.scss';
+import { PageHome, Aside, AsideContent, Main, MainContent, LoginButton} from './styles';
 
 export function Home(){
 
@@ -19,28 +19,28 @@ export function Home(){
     }
 
     return(
-        <div id="page-home">
-            <aside>
-                <div className="aside-content">
+        <PageHome>
+            <Aside>
+                <AsideContent>
                     <p>
                     “Não siga a estrada, apenas; <strong>ao contrário</strong>. Vá por onde não haja estrada e deixe uma <strong>trilha</strong>.”
                     </p>
                     <span>- Muriel Strode</span>
-                </div>
-            </aside>
-            <main>
-                <div className="main-content">
+                </AsideContent>
+            </Aside>
+            <Main>
+                <MainContent>
                     <img src={logoHomeImg} alt="Treelha" />
-                    <button className="login-button" onClick={handleSignInWithGoogle}>
+                    <LoginButton onClick={handleSignInWithGoogle}>
                         <img src={googleImg} alt="Logo google" />
                         Fazer login com o google
-                    </button>
+                    </LoginButton>
                     <span>For better experience, add a profile picture to your Google account.</span>
+                </MainContent>
                 <footer>
                     <p>2021 <strong>Treelha</strong> &copy; All rights reserved</p>
                 </footer>
-                </div>
-            </main>
-        </div>
+            </Main>
+        </PageHome>
     )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+import backgroundImage from '../../assets/images/bg-home.jpg';
+
+export const PageHome = styled.div`
+    display: flex;
+    align-items: stretch;
+    height: 100vh;
+`;
+
+export const Aside = styled.aside`
+    flex: 7;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    background:  var(--home-gradient), url(${backgroundImage}) no-repeat;
+    background-size: 100%;
+    background-position: 10% 90%;
+`;
+
+export const AsideContent = styled.div`
+    flex: 7;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    gap: 5px;
+    margin-left: 44px;
+
+    p {
+        max-width: 532px;
+    }
+
+    color: #FFFFFF;
+    font-weight: 600;
+    font-size: 36px;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.5);
+
+    strong {
+        color: var(--water-green);
+    }
+
+    span {
+        font-style: italic;
+        font-weight: normal;
+        font-size: 24px;
+    }
+`;
+
+export const Main = styled.main`
+    flex: 8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    footer {
+        position: fixed;
+        margin-bottom: 10px;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-size: 14px;
+
+        strong {
+            color: var(--bright-green);
+        }
+    }
+`;
+
+export const MainContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 37px;
+
+    span {
+        text-align: center;
+        max-width: 300px;
+        font-size: 12px;
+        color: var(--green);
+    }
+`;
+
+export const LoginButton = styled.button`
+    width: 300px;
+    height: 50px;
+    border-radius: 8px;
+    font-weight: 500;
+    background-color: #ea4335;
+    color: #fff;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    cursor: pointer;
+    border: 0;
+
+    transition: filter 0.2s;
+
+    img {
+        margin-right: 8px;
+    }
+
+    &:hover {
+        filter: brightness(0.9);
+    }
+`;
