63e504999d6019705f7d6846f8161e6bd519c4d5
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 11:19:11 -0300

    Refactoring Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,35 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
-
+import { Modal } from '../Modal';
 import './styles.scss';
+
+
+
 
 export function Header(){
 
     const { user } = useAuth();
 
+    const [showModal, setShowModal] = useState(false);
+
     return(
+        <>
         <div className="header">
-            <motion.h1
-             whileHover={{scale: 1.1, color: '#29B672'}}
-             whileTap={{scale: 0.9, color: '#96C67F'}}
-            >Treelha</motion.h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Minhas trilhas</li>
-                </ul>
-            </nav>
-            <div className="user-info">
-                <p className="user-name">{user?.name}</p>
-                <img src={user?.avatar} alt={user?.name} />
-            </div>
-            
+            <div className="content">
+                <motion.h1
+                whileHover={{scale: 1.1, color: '#29B672'}}
+                whileTap={{scale: 0.9, color: '#96C67F'}}
+                >Treelha</motion.h1>
+                <div className="user-info" onClick={() => setShowModal(!showModal)}>
+                    <div className="user-info-content">
+                        <p className="user-name">{user?.name}</p>
+                        <img className="avatar" src={user?.avatar} alt={user?.name} /> 
+                    </div>         
+                    <Modal showModal={showModal} setShowModal={setShowModal}/>
+                </div>
+            </div>  
         </div>
+        </>
     )
 }
