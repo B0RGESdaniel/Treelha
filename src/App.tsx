61418089f43afa7aa1d9db2b21a1c3e2ed413e89
B0RGESdaniel <78994156+B0RGESdaniel@users.noreply.github.com>
2021-11-01 11:19:44 -0300

    Adding new dependencies and SVG images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { MyTreelhas } from './pages/MyTreelhas';
+import { NewTreelha } from './pages/NewTreelha';
 import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/treelhas" component={MyTreelhas}/>
+          <Route path="/treelhas" exact component={MyTreelhas}/>
+          <Route path="/treelhas/newtreelha" component={NewTreelha}/>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
